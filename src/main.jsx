6ce1e4534f84b0d1e9528bd7a2020e9ec12e175d
dipjoy01 <mypc08538@gmail.com>
2026-06-07 17:54:26 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import Cart from "@/pages/cart";
